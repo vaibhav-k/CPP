792dce1d6cb7b3267b045cc7fd325c0b8e266bdf
Vaibhav Kulshrestha <vaibhav1kulshrestha@gmail.com>
2017-03-04 22:43:38 +0530

    Simple compund interest calculator

--- Financial Mathematics/InterestCalculator/main.cpp
+++ Financial Mathematics/InterestCalculator/main.cpp
@@ -3,20 +3,28 @@
 
 using namespace std;
 
+double compoundInterest(double P, double i, double n)
+{
+    double interest = P * pow(1 + 0.01*i, n) - P;
+    return interest;
+}
+
 int main()
 {
     int principal;
     double interestRate;
     int numberOfYears;
+
     cout <<"How much are you investing?\n";
     cin >>principal;
     cout <<"What is the annual interest rate(%)?\n";
     cin >>interestRate;
     cout <<"How long for(years)?\n";
     cin >>numberOfYears;
-    double finalBalance = pow(1.0 + interestRate * 0.01, numberOfYears) * principal;
-    double interest = finalBalance - principal;
-    cout <<"You will earn";
+
+    double interest = compoundInterest(principal, interestRate, numberOfYears);
+
+    cout <<"You will earn ";
     cout <<interest ;
     cout <<"\n";
 
